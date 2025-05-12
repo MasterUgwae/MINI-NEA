ecccd8b7591b8ee8af185fa7c480cf21035b712b
MasterUgwae <branmf93@gmail.com>
2025-05-12 11:49:07 +0100

    slightly broken

--- main.py
+++ main.py
@@ -1,10 +1,45 @@
 # main.py
 
+import os
+import sys
+
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
+
+
+
+
 import pygame
 
 
-class Block:
+class Clickable:
 
+
+
+    def __init__(self, x, y, width, height):
+        self.x = y
+        self.y = y
+        self.width = width
+        self.height = height
+
+    def draw(self, screen):
+        pygame.draw.rect(screen, (255, 0, 0), (self.x, self.y, self.width, self.height))
+
+class Tile(Clickable):
+
+    def __init__(self, x, y, grid_tile_width, grid_tile_height):
+        #x, y are the tiles position on the grid and not the screen
+        super().__init__(x, y, grid_tile_width, grid_tile_height)
+    
+    def draw(self, screen, angel_x, angel_y, angel_power, ):
+        if (angel_x + angel_power <= self.x and angel_x - angel_power >= self.x and 
+                angel_y + angel_power <= self.y and angel_y - angel_power >= self.y):
+            top = self.y - self.height // 2
+            left = self.x - self.width // 2
+            rect = pygame.Rect(left, top, self.width, self.height)
+            
+
+
+class Block:
 
     def __init__(self) -> None:
         self.x = 0
@@ -17,20 +52,17 @@ class Block:
                 self.y >= grid_center_y - grid_height // 2 and
                 self.y <= grid_center_y + grid_height // 2)
 
-    def render_block(self, screen, grid_center_x, grid_center_y, grid_height, grid_width):
+    def draw(self, screen, grid_center_x, grid_center_y, grid_height, grid_width):
         block_width = grid_width // 10
         block_height = grid_height // 10
         block_x = grid_center_x - grid_width // 2 + (self.x * block_width)
         block_y = grid_center_y - grid_height // 2 + (self.y * block_height)
         pygame.draw.rect(screen, (125, 19, 19), (block_x, block_y, block_width, block_height))
 
-class Button:
+class Button(Clickable):
 
     def __init__(self, x, y, width, height, text):
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
+        super().__init__(x, y, width, height)
         self.text = text
 
     def draw(self, screen):
@@ -39,6 +71,7 @@ class Button:
         text_surface = font.render(self.text, True, (0, 0, 0))
         text_rect = text_surface.get_rect(center=(self.x, self.y))
         screen.blit(text_surface, text_rect)
+
 
 class GameState:
 
@@ -51,6 +84,7 @@ class GameState:
         self.grid_centre_y = 0
         self.screen = None
         self.clock = None
+        self.blocks = []
 
     def add_clock(self, clock):
         self.clock = clock
@@ -58,9 +92,11 @@ class GameState:
     def add_screen(self, screen):
         self.screen = screen
 
+    def add_block(self, block):
+        self.blocks.append(block)
 
 
-def Main():
+def main():
     game_state = GameState()
 
     pygame.init()
@@ -72,15 +108,14 @@ def Main():
     game_state.add_screen(screen)
     game_state.add_clock(clock)
 
-    end = StartScreen(game_state)
-    print("End of StartScreen")
+    end = startScreen(game_state)
 
     while not end:
-        Menu(game_state.screen)
+        menu(game_state)
 
-    ExitGame()
+    exitGame()
 
-def StartScreen(game_state):
+def startScreen(game_state):
     # Fill half of the screen with white and the other half with red
     SCREEN_WIDTH = game_state.SCREEN_WIDTH
     SCREEN_HEIGHT = game_state.SCREEN_HEIGHT
@@ -89,10 +124,13 @@ def StartScreen(game_state):
     screen.fill((255, 255, 255))
     half_screen = SCREEN_WIDTH // 2
     pygame.draw.rect(screen, (255, 0, 0), pygame.Rect(0,0, half_screen, SCREEN_HEIGHT))
+    title = pygame.font.Font(None, 74).render("Angel Problem", True, (0, 0, 0))
+    title_rect = title.get_rect(center=(half_screen // 2, SCREEN_HEIGHT // 2 - 50))
+    screen.blit(title, title_rect)
     startButton = Button(half_screen, SCREEN_HEIGHT // 2 - 25, 100, 50, "Start")
     start = False
     end = False
-    while not start or not end:
+    while not (start or end):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 end = True
@@ -113,37 +151,70 @@ def StartScreen(game_state):
         return False
 
 
-def Menu(screen):
-    screen.fill((0,0,0))
+def menu(game_state):
+    SCREEN_WIDTH = game_state.SCREEN_WIDTH
+    SCREEN_HEIGHT = game_state.SCREEN_HEIGHT
+    clock = game_state.clock
+    screen = game_state.screen
+    ClearScreen(screen, (230, 230, 230))
+    play_button = Button(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2 - 25, 100, 50, "Play")
+    options_button = Button(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2 + 25, 100, 50, "Options")
+    exit_button = Button(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2 + 75, 100, 50, "Exit")
+    end = False
+    start = False
+    while not(end or start):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                end = True
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                print("hi")
+                mouse_x, mouse_y = event.pos
+                if (play_button.x <= mouse_x <= play_button.x + play_button.width and
+                    play_button.y <= mouse_y <= play_button.y + play_button.height):
+                    start = True
+                elif (options_button.x <= mouse_x <= options_button.x + options_button.width and
+                        options_button.y <= mouse_y <= options_button.y + options_button.height):
+                    Options()
+                elif (exit_button.x <= mouse_x <= exit_button.x + exit_button.width and
+                        exit_button.y <= mouse_y <= exit_button.y + exit_button.height):
+                    end = True
+                    print("Exiting...")
+        ClearScreen(screen)
+        play_button.draw(screen)
+        options_button.draw(screen)
+        exit_button.draw(screen)
+        pygame.display.update()
+        clock.tick(60)
 
-def Options():
+
+def options():
     pass
 
-def Gameloop():
+def gameloop():
     pass
 
-def HandleInput():
+def handleInput():
     pass
 
-def RenderGrid():
+def renderGrid():
     pass
 
-def MoveGrid():
+def moveGrid():
     pass
 
-def GetBlockedTiles():
+def getBlockedTiles():
     pass
 
-def PlaceBlockedTile():
+def placeBlockedTile():
     pass
 
-def FindLegalMoves():
+def findLegalMoves():
     pass
 
-def HighlightLegalMoves():
+def highlightLegalMoves():
     pass
 
-def CheckLegalMove():
+def checkLegalMove():
     pass
 
 def undoMove():
@@ -152,11 +223,12 @@ def undoMove():
 def redoMove():
     pass
 
-def ClearScreen(screen):
-    screen.fill((255, 255, 255))
+def clearScreen(screen,colour=(255, 255, 255)):
+    screen.fill(colour)
 
-def ExitGame():
+def exitGame():
     pygame.quit()
+    sys.exit()
 
 if __name__=="__main__":
     Main()
